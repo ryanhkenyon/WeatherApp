99b06a86dd7d72459eabf0699f4ae725ed0d3a33
KienOda <binhaccount@gmail.com>
2022-08-15 02:16:08 -0400

    updated JS with more API code
    -Fixed API to work properly
    
    -Changed Rain to humidity as API does not show rain chance
    
    -Weather icon changes based on weather report
    
    -Weather message changes based on weather report
    
    -Remove state from location
    
    -removed useless comments

--- js/ourScript.js
+++ js/ourScript.js
@@ -3,7 +3,6 @@ let button = document.getElementById('button');
 button.addEventListener("click", function search() {
     
     let zipCode = document.getElementById("input");
-    console.log(zipCode.value);
     
     if (zipCode.value > 9999 && zipCode.value < 100000) {
         //Grab elements
@@ -12,65 +11,80 @@ button.addEventListener("click", function search() {
         let cityName = document.getElementById("cityName");
         let currentTemp = document.getElementById("currentTemp");
         let lowHighTemp = document.getElementById("lowHighTemperature");
-        let chanceOfRain = document.getElementById("chanceOfRain");
         let weatherMessage = document.getElementById("weatherMessage");
         let days = document.getElementById("days").getElementsByTagName("div");
-        // let humidity = document.getElementById("humidity");
-        // let clouds = document.getElementById("clouds");
+        let humidity = document.getElementById("humidity");
         // let windSpeed = document.getElementById("windSpeed");
         // let feelsLike = document.getElementById("feelsLike");
         let weekInfo = [];
 
         fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode.value}&appid=7df71b8a7701d4f29648eebc701ed349`)
-            .then(locationApiData => {
+            .then((locationApiData) => {
                 return locationApiData.json();
             })
-            .then(data => {
-                cityName.textContent = data.name;
-                let lat = data.lat;
-                let lon = data.lon;
-                
-                fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely&hourly&appid=7df71b8a7701d4f29648eebc701ed349`)
-                .then(weatherApiData => {
-                    // console.log(weatherApiData);
+            .then((zipCode) => {
+                if (zipCode.lat !== undefined && zipCode.lon !== undefined) {
+                    cityName.textContent = zipCode.name;
+                    return zipCode;
+                } 
+            })
+            .then((zipCodeData) => {
+                fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${zipCodeData.lat}&lon=${zipCodeData.lon}&units=imperial&exclude=current,minutely&hourly&appid=7df71b8a7701d4f29648eebc701ed349`)
+                .then((weatherApiData) => {
                     return weatherApiData.json();
                 })
-                .then(weatherData => {  
-                    // console.log(weatherData.daily[0]);
+                .then((weatherData) => {
                     for (let x = 0; x < 7; x++) {
-                        // days[x].innerHTML = `<p><strong>Day ${x}</strong> L: ${weatherData.daily[x].temp.min} / H: ${weatherData.daily[x].temp.max}</p>`;
-                        // lowHighTemp.innerText = `Low ${weatherData.daily[0].temp.min} / High ${weatherData.daily[0].temp.max}`;
                         weekInfo.push(weatherData.daily[x]);
-
-                        // let unix_timestamp = 1660582800;
-                        // // Create a new JavaScript Date object based on the timestamp
-                        // // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-                        // var date = new Date(unix_timestamp * 1000);
-                        // // Hours part from the timestamp
-                        // var hours = date.getHours();
-                        // // Minutes part from the timestamp
-                        // var minutes = "0" + date.getMinutes();
-                        // // Seconds part from the timestamp
-                        // var seconds = "0" + date.getSeconds();
-                        // console.log(date.getDate());
-                        // console.log(date.getMonth() + 1);
-                        // console.log(date.getDay());
                     }
-                    // currentTemp.innerText = weatherData.daily[0].temp.day;
-                    // feelsLike.innerText = ${weatherData.daily[0].feels_like.day};
-                    // humidity.innerText = weatherData.daily[0].humidity;
-                    // clouds.innerText = weatherData.daily[0].clouds;
-                    // windSpeed.innerText = weatherData.daily[0].wind_speed;
-                    
-                    
+                    return weekInfo;
                 })
-                //catching something unknown but code runs
-                .catch(console.log("WeatherDataAPI Catch"));
-            })
-            .catch(console.log("Catch occured!"));
+                .then((weekInfo) => {
+                    for (let x = 0; x < days.length; x++) {
+                        let unix_timestamp = weekInfo[x].dt;
+                        // Create a new JavaScript Date object based on the timestamp
+                        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+                        let date = new Date(unix_timestamp * 1000);
+                        let month = date.getMonth() + 1;
+                        let day = date.getDate();
+                        let dayOfWeek = date.getDay();
+                        let year = date.getFullYear();
+                        let dayHTML;
 
+                        if (x === 0) {
+                            selectedDate.innerText = `${month}/${day}/${year}`;
+                            currentTemp.innerText = `${weekInfo[x].temp.day.toFixed(0)}\u00B0F`;
+                            lowHighTemp.innerText = `Low: ${weekInfo[x].temp.min.toFixed(0)}\u00B0F / High: ${weekInfo[x].temp.max.toFixed(0)}\u00B0F`;
+                            weatherMessage.innerText = weekInfo[x].weather[0].description;
+                            humidity.innerText = `Humidity: ${weekInfo[x].humidity}%`
+                            weatherIcon.src = `http://openweathermap.org/img/wn/${weekInfo[x].weather[0].icon}@2x.png`;
+                        }
+
+                        if (dayOfWeek === 0) {
+                            dayHTML = `<strong>Sunday</strong>`;
+                        } else if (dayOfWeek === 1) {
+                            dayHTML = `<strong>Monday</strong>`;
+                        } else if (dayOfWeek === 2) {
+                            dayHTML = `<strong>Tuesday</strong>`;
+                        } else if (dayOfWeek === 3) {
+                            dayHTML = `<strong>Wednesday</strong>`;
+                        } else if (dayOfWeek === 4) {
+                            dayHTML = `<strong>Thursday</strong>`;
+                        } else if (dayOfWeek === 5) {
+                            dayHTML = `<strong>Friday</strong>`;
+                        } else if (dayOfWeek === 6) {
+                            dayHTML = `<strong>Saturday</strong>`;
+                        }
+                        
+                        days[x].innerHTML = `<p>${dayHTML}<br>${month}/${day}/${year}<br>L: ${weekInfo[x].temp.min.toFixed(0)}\u00B0F / H: ${weekInfo[x].temp.max.toFixed(0)}\u00B0F</p>`;
+                    }
+                })
+                .catch((error) => {
+                    console.log(`WeatherDataAPI Catch ${error}`);
+                });
+            })
+        .catch((error) => {
+            console.log(`Location API Data error: ${error}`);
+        });
     }            
 });
-
-
-
